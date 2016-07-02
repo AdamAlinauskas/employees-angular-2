e562d54990d9c60aca911ebdbd5ca86171884cbd
Adam Alinauskas <aalinauskas@gmail.com>
2016-07-02 12:22:44 -0600

    Listing added employees

--- app/employees.component.ts
+++ app/employees.component.ts
@@ -1,10 +1,25 @@
-import {Component} from '@angular/core'
+import {Component,OnInit} from '@angular/core'
+import {EmployeeTasks} from './tasks/employee-tasks'
+import {Employee} from './domain/employee'
 
 @Component({
     selector:'employees',
     templateUrl:'app/templates/employees.component.html'
 
 })
-export class EmployeesComponent {
 
+export class EmployeesComponent implements OnInit {
+    me:EmployeesComponent = this;
+    employees:Employee[] = [];
+
+    constructor(public employeeTasks: EmployeeTasks){
+
+    }
+
+    ngOnInit(){
+        this.employeeTasks.getAllEmployees().then((allEmployees)=> {
+            this.employees = allEmployees
+            }
+        );
+    }
 }
--- app/tasks/employee-tasks.ts
+++ app/tasks/employee-tasks.ts
@@ -2,11 +2,15 @@ import {Employee} from "../domain/employee";
 
 export class EmployeeTasks{
 
-    employees: Employee[] = Employee[0];
+    employees: Employee[] = [];
 
     //return a promoise for now. This will allow us to refactor to using a web service.
     save(employee:Employee){
         this.employees.push(employee);
         return Promise.resolve();
+    }
+
+    getAllEmployees(){
+        return Promise.resolve(this.employees);
     }
 }
